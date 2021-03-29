46ab41728e9770727f939cab6e73583e3b7c5c11
Dominic Spitzig <45005804+Domino88876YouTube@users.noreply.github.com>
2021-03-29 11:22:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.Yes)
+        basic.showString("Walk")
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.No)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.showIcon(IconNames.No)
+        basic.showString("Don't Walk")
+        basic.pause(5000)
+    }
+})
